103be9db5a77807c3019c500e9c2ba407f19c285
Hoàng 2363 <127336670+Hoang-2363@users.noreply.github.com>
2025-05-25 05:34:29 +0700

    Update WebConfig.java

--- src/main/java/com/backend/config/WebConfig.java
+++ src/main/java/com/backend/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 //                .allowedOrigins("https://donghophuongnam23.web.app")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://eclectic-figolla-205ee4.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
